23fa1455a9106a728c26a9d0b544b2d5761501e7
Woon Hao <yongwh99@hotmail.com>
2021-12-27 20:19:11 +0800

    Privacy update

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 # birthday-present
 ## Background
-This is a personal project I made as a birthday present to a friend of mine. It is a combination of a Telegram chatbot and a webpage.
+This is a personal project I made as a birthday present to a close friend of mine. It is a combination of a Telegram chatbot and a webpage.
+
+**Note: Most of the commits have been squashed into the first commit as they contained information related to my friends.**
 
 ### Telegram Bot
+* Written in Python
 * Hosted on Heroku
-* Uses a webhook on Heroku (app url as a reverse proxy) for updates
+* Uses a webhook on Heroku (app url acts as a reverse proxy) for updates
 * Sensitive information stored as environment variables on Heroku
 * GitHub repo integrated with Heroku to automatically deploy on push to **main** branch
-* Bot will ask for user's information to validate identity
+* Bot will ask for user's credentials (email, password) to validate identity
 * Upon validation, the link to the webpage will be sent to the user
 
 ### Webpage
 * Hosted on Netlify
 * Uses HTML, CSS, JavaScript, jQuery, Bootstrap
-* 3 different scrolls
+* 3 different scroll pages
     * 1st - Present + Cake
     * 2nd - Carousel of photos
     * 3rd - Well-wishes from friends
--- birthday_bot.py
+++ birthday_bot.py
@@ -17,10 +17,9 @@ PORT = int(os.environ.get("PORT", "8443"))
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
 
 def start(update: Update, context: CallbackContext):
-    reply = "Hello! This is a bot created by Woon Hao. Enter /help for assistance. Also, happy birthday XZ!"
+    reply = "Hello! This is a bot created by Woon Hao. Enter /help for assistance. Also, happy birthday!"
     update.message.reply_text(reply)
-    update.message.reply_text("HAHAHHA")
-    input = "pls key in your school email with faculty eg. @business.smu.edu.sg"
+    input = "please key in your school email"
     update.message.reply_text(input)
     return 1
 
@@ -30,39 +29,36 @@ def end(update: Update, context: CallbackContext):
 
 def help(update: Update, context: CallbackContext):
     update.message.reply_text("Use /start to restart or /end to finish the conversation at any point in time!")
-    update.message.reply_text("can text me (woon hao) if there's any issues w the bot ty")
+    update.message.reply_text("you can text me (woon hao) if there's any issues with the bot, thank you!")
 
 def get_email(update: Update, context: CallbackContext):
     input_email = update.message.text
-    alt_correct_email = "email"
-    correct_email = "xinzi.ho.2020@business.smu.edu.sg"
-    if (input_email == correct_email) or (input_email == alt_correct_email):
-        update.message.reply_text("now key in your password (hint: got 'love' and 'flower' in the pw)")
+    correct_email = "email"
+    if input_email == correct_email:
+        update.message.reply_text("now, key in your password!")
         return 2
     elif input_email == "/end":
         update.message.reply_text("BYE!")
         return -1
     else:
-        update.message.reply_text("incorrect email, help la own email also dk")
+        update.message.reply_text("incorrect email, please try again")
         return 1
 
 def get_password(update: Update, context: CallbackContext):
     input_password = update.message.text
-    alt_correct_password = "password"
-    correct_password = "LOVEflower112!"
-    url = "https://birthday-present-webpage.netlify.app/"
+    correct_password = "password"
+    url = "URL of website"
     
-    if (input_password == correct_password) or (input_password == alt_correct_password):
-        update.message.reply_text("ok identity confirmed")
+    if input_password == correct_password:
+        update.message.reply_text("ok, identity confirmed")
         update.message.reply_text(url)
-        update.message.reply_text("access the above link with a laptop thanks")
-        update.message.reply_text("in the website, open the present and scroll down")
+        update.message.reply_text("the link above is best accessed using a laptop")
         return ConversationHandler.END
     elif input_password == "/end":
         update.message.reply_text("BYE!")
         return -1
     else:
-        update.message.reply_text("incorrect password, pls it's your own password hello")
+        update.message.reply_text("incorrect password, please try again")
         return 2
 
 def main():
